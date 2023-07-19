6f0c77eff963f14f82d561faaa878401a692798e
Manjari U <75825660+ManjariU@users.noreply.github.com>
2023-07-19 12:40:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA"
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#EFD81D"
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAF8"
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33"
+  }
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="profilejpg.jpg" alt="Manjari U" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Manjari U</h1>
+      <p>
+        An Ambitious Front-End Developer who is a fresh computer science
+        undergraduate. Looking for new and challenging oppurtunities to conquer
+        them and thoroughly learning everything.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
